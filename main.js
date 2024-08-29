e079fcd6911a333aa1832294fd358da2f66b4a3b
francolloveras <francorlloveras@gmail.com>
2024-08-28 23:16:20 -0300

    Change 'element' to '$' and getElementById to querySelector

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ const jsConfetti = new JSConfetti();
 const rawWords = await fetch("./words-es.json");
 const words = await rawWords.json();
 
-const paragraphElement = document.querySelector("p");
-const inputElement = document.querySelector("input");
-const timeElement = document.querySelector("time");
-const dialogElement = document.querySelector("dialog");
-const restartButtonElement = document.querySelector("button");
+const $paragraph = document.querySelector("p");
+const $input = document.querySelector("input");
+const $time = document.querySelector("time");
+const $dialog = document.querySelector("dialog");
+const $restartButton = document.querySelector("button");
 
 // Defining utils constants variables.
 const INITIAL_TIME = 5;
@@ -31,24 +31,24 @@ class TypingGame {
   }
 
   init = () => {
-    paragraphElement.innerHTML = "";
+    $paragraph.innerHTML = "";
 
     words.forEach((word, index) => {
-      const wordElement = document.createElement("word");
+      const $word = document.createElement("word");
       word.split("").forEach((letter) => {
-        const letterElement = document.createElement("letter");
-        letterElement.textContent = letter;
-        wordElement.appendChild(letterElement);
+        const $letter = document.createElement("letter");
+        $letter.textContent = letter;
+        $word.appendChild($letter);
       });
 
       if (index === 0) {
-        wordElement.classList.add("current");
+        $word.classList.add("current");
       }
 
-      paragraphElement.appendChild(wordElement);
+      $paragraph.appendChild($word);
     });
 
-    timeElement.textContent = formatTime(INITIAL_TIME);
+    $time.textContent = formatTime(INITIAL_TIME);
   };
 
   start = () => {
@@ -56,7 +56,7 @@ class TypingGame {
       this.gameStart = true;
       const countdownInterval = setInterval(() => {
         this.timeLeft--;
-        timeElement.textContent = formatTime(this.timeLeft);
+        $time.textContent = formatTime(this.timeLeft);
 
         // End the game when time reaches zero.
         if (this.timeLeft < 0) {
@@ -68,17 +68,17 @@ class TypingGame {
   };
 
   update = ({ key, value }) => {
-    const currentWord = document.querySelector("word.current");
-    const successTyped = value.toLowerCase() === currentWord.textContent.toLowerCase();
+    const $currentWord = document.querySelector("word.current");
+    const successTyped = value.toLowerCase() === $currentWord.textContent.toLowerCase();
 
     // If key pressed is Space, change the current word.
     if (key === SPACE_KEY && value !== "") {
       // Update word's class based on correctness.
-      currentWord.classList.add(successTyped ? "correct" : "incorrect");
+      $currentWord.classList.add(successTyped ? "correct" : "incorrect");
 
       // Remove current highlight and apply it to the next word.
-      currentWord.classList.remove("current");
-      currentWord.nextElementSibling.classList.add("current");
+      $currentWord.classList.remove("current");
+      $currentWord.nextElementSibling.classList.add("current");
     }
 
     // If key pressed is not space, save the keystroke letter.
@@ -88,38 +88,38 @@ class TypingGame {
   };
 
   setScore = () => {
-    const wordsPerMinuteElement = document.getElementById("words-per-minute");
-    const correctWordsElement = document.getElementById("correct-words");
-    const incorrectWordsElement = document.getElementById("incorrect-words");
-    const totalKeystrokesElement = document.getElementById("total-keystrokes");
-    const correctKeystrokesElement = document.getElementById("correct-keystrokes");
-    const incorrectKeystrokesElement = document.getElementById("incorrect-keystrokes");
-    const accuracyElement = document.getElementById("accuracy");
+    const $wordsPerMinute = document.querySelector("#words-per-minute");
+    const $correctWords = document.querySelector("#correct-words");
+    const $incorrectWords = document.querySelector("#incorrect-words");
+    const $totalKeystrokes = document.querySelector("#total-keystrokes");
+    const $correctKeystrokes = document.querySelector("#correct-keystrokes");
+    const $incorrectKeystrokes = document.querySelector("#incorrect-keystrokes");
+    const $accuracy = document.querySelector("#accuracy");
 
     const totalIncorrectWords = document.querySelectorAll("word.incorrect").length;
     const totalCorrectWords = document.querySelectorAll("word.correct").length;
     const totalWordsPerMinute = (totalCorrectWords * 60) / this.time;
 
-    wordsPerMinuteElement.textContent = Math.round(totalWordsPerMinute);
-    correctWordsElement.textContent = totalCorrectWords;
-    incorrectWordsElement.textContent = totalIncorrectWords;
+    $wordsPerMinute.textContent = Math.round(totalWordsPerMinute);
+    $correctWords.textContent = totalCorrectWords;
+    $incorrectWords.textContent = totalIncorrectWords;
 
     const totalKeystrokes = this.score.keystrokes.length;
     const correctKeystrokes = this.score.keystrokes.filter((keystroke) => keystroke).length;
     const incorrectKeystrokes = totalKeystrokes - correctKeystrokes;
 
-    totalKeystrokesElement.textContent = totalKeystrokes;
-    correctKeystrokesElement.textContent = correctKeystrokes;
-    incorrectKeystrokesElement.textContent = incorrectKeystrokes;
+    $totalKeystrokes.textContent = totalKeystrokes;
+    $correctKeystrokes.textContent = correctKeystrokes;
+    $incorrectKeystrokes.textContent = incorrectKeystrokes;
 
     const accuracy = (correctKeystrokes / totalKeystrokes) * 100;
-    accuracyElement.textContent = `${accuracy}%`;
+    $accuracy.textContent = `${accuracy}%`;
   };
 
   end = () => {
     jsConfetti.addConfetti();
     this.setScore();
-    dialogElement.showModal();
+    $dialog.showModal();
   };
 
   restart = () => {
@@ -129,7 +129,7 @@ class TypingGame {
       keystrokes: [],
     };
     this.init();
-    dialogElement.close();
+    $dialog.close();
   };
 }
 
@@ -137,22 +137,22 @@ const game = new TypingGame({ words, time: INITIAL_TIME });
 game.init();
 
 // Add keydown event listener to the input.
-inputElement.addEventListener("keydown", (event) => {
+$input.addEventListener("keydown", (event) => {
   if (event.ctrlKey || event.altKey || event.shiftKey) return;
 
   // If a key was pressed, start the game.
   game.start();
 
   // Update the game with the value word.
-  const trimmedValue = inputElement.value.trim();
+  const trimmedValue = $input.value.trim();
   game.update({ key: event.key, value: trimmedValue });
 
   // if the spacebar is pressed clear the input value.
   if (event.key === SPACE_KEY) {
-    inputElement.value = "";
+    $input.value = "";
   }
 });
 
-restartButtonElement.addEventListener("click", () => {
+$restartButton.addEventListener("click", () => {
   game.restart();
 });
